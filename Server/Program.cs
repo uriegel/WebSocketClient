f4c658f52bf4ff20559cbef14f695eb05e143c00
uriegel <uriegel@hotmail.de>
2025-07-19 10:44:19 +0200

    WebSocketClient

---
+++ Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using WebServerLight;
+
+using static System.Console;
+
+var server =
+    WebServer
+        .New()
+        .Http(8080)
+        .WebSocket(WebSocket)
+        .Build();
+    
+server.Start();
+ReadLine();
+server.Stop();
+
+static async void WebSocket(IWebSocket webSocket)
+{
+    var i = 0;
+    while (true)
+    {
+        await Task.Delay(5000);
+        await webSocket.SendString($"Web socket text: {++i}");
+    }
+}
+
+
